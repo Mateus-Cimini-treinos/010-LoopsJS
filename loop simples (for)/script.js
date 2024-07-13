e91646bf2e934c293f28b0d3b03ddbf1a1baa3c6
Mateus Cimini Araujo <mateusmateus122333@gmail.com>
2024-07-13 20:49:00 -0300

    add new repository

---
+++ loop simples (for)/script.js
@@ -0,0 +1,9 @@
+// loop simples que defini quantas vezez uma coisa vai acontecer usando for
+
+
+console.log("Vai começar...") // mostrar a mensagem vai começar no console do NodeJs
+for(var contador = 1;contador <= 10;contador++) { // começa o for criando variavel contador e atribuindo valor 1 enquanto variavel contador for menor ou igual a 10  e adiciona 1 no valor de contador
+    console.log(contador)  // mostrar a mensagem com o numero do valor da variavel contador no console do NodeJs
+}
+
+console.log("Terminou!!!") // mostrar a mensagem terminou no console do NodeJs
---
+++ loop simples js (while)/script.js
@@ -0,0 +1,17 @@
+// loop simples que defini quantas vezez uma coisa vai acontecer while
+
+
+var contador = 1  //atribuir a variavel contador com 6
+while (contador <= 6) {  // enquanto variavel contador for menor ou igual a 6 fazer:
+     console.log("Ola")   // mostrar a mensagem ola no console do NodeJs
+     contador++ // acrecenta 1 ao valor da variavel contador a cada loop
+} // fecha bloco
+
+
+/*
+console.log("ola")
+console.log("ola")
+console.log("ola")
+console.log("ola")
+console.log("ola")
+*/
